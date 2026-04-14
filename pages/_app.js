fbc1328b4b1a053a334fd45a123266e2b67fa13c
Rede Sabrina <bruno.redesabrina@gmail.com>
2026-04-14 13:44:30 -0300

    add/favicon.ico

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ export default function App({ Component, pageProps }){
       <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link href="https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;600;700&family=Playfair+Display:ital,wght@0,700;1,400&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
